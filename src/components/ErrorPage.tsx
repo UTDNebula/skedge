eb2d6c641709ffde7e14b1e0b9890625a2750e91
Alexis Kaufman <alexiskaufman@gmail.com>
2023-08-27 13:56:03 -0500

    remove re-fetch button

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -15,11 +15,6 @@ export default function ErrorPage(props: ErrorProps) {
     <>
       <header className="h-10 rounded-t-2xl bg-red-dark py-2 pr-3 pl-[14px] flex">
         <h3 className="text-white">Err: {error.source}</h3>
-        {/* <button onClick={() => console.log("Fixme!")} className="ml-auto">
-          <h2 className="px-1.5 hover:bg-red-dark-hover rounded-lg transition duration-250 ease-in-out text-white">
-            ⟳
-          </h2>
-        </button> */}
       </header>
       <Card>
         <div className="grid grid-cols-12">
