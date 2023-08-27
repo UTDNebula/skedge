974b1608c61e96c0f3223759533ddd2524237af2
Alexis Kaufman <alexiskaufman@gmail.com>
2023-08-27 13:44:07 -0500

    use better error structure. hide refresh button because we do not have feasible implementation for it.

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,30 +1,28 @@
 import { Card } from "./Card"
 
-function fixme() {
-  console.log("Fix me!")
+type Error = {
+  error: {
+    source: string
+    details: string
+  }
 }
 
-type ErrorPageProps = {
-  source: string
-  error: string
-}
-
-export default function ErrorPage(props: ErrorPageProps) {
+export default function ErrorPage(props: Error) {
+  const { error } = props
   return (
     <>
       <header className="h-10 rounded-t-2xl bg-red-dark py-2 pr-3 pl-[14px] flex">
-        <h3 className="text-white">Errors Found.</h3>
-        <button onClick={fixme} className="ml-auto">
+        <h3 className="text-white">Err: {error.source}</h3>
+        {/* <button onClick={() => console.log("Fixme!")} className="ml-auto">
           <h2 className="px-1.5 hover:bg-red-dark-hover rounded-lg transition duration-250 ease-in-out text-white">
             ⟳
           </h2>
-        </button>
+        </button> */}
       </header>
       <Card>
         <div className="grid grid-cols-12">
           <div className="col-span-12">
-            <h3>{props.source}</h3>
-            <p>err: {props.error}</p>
+            <p>{error.details}</p>
           </div>
         </div>
       </Card>
