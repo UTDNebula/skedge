12fec1e12182d9bfdba326585b7e70c576499745
Alexis Kaufman <alexiskaufman@gmail.com>
2023-08-27 13:44:34 -0500

    reformat

--- src/background/messages/getScrapeData.ts
+++ src/background/messages/getScrapeData.ts
@@ -1,10 +1,10 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
-import { getScrapedCourseData } from "../../background";
- 
+
+import { getScrapedCourseData } from "../../background"
+
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
-  const data = await getScrapedCourseData();
+  const data = await getScrapedCourseData()
   res.send(data)
-  // res.send(null)
 }
- 
+
 export default handler
