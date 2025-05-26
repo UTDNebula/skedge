1456cfc22cb1455dbf55dc73a2784aff9fc3d13c
Tyler Hill <gotyhill@gmail.com>
2025-05-25 23:14:52 -0500

    Bug Report

--- src/tabs/permissions.tsx
+++ src/tabs/permissions.tsx
@@ -12,9 +12,9 @@ Sentry.init({
   // Add optional integrations for additional features
   integrations: [
     Sentry.replayIntegration(),
-    /*Sentry.feedbackIntegration({
+    Sentry.feedbackIntegration({
       showBranding: false,
-    }),*/
+    }),
   ],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
