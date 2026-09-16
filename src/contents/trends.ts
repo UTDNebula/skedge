8de8a8902a9c442add6889c2284b1f44840eac31
davis118 <davismo118@gmail.com>
2026-09-16 15:10:17 -0500

    filter for valid classes and store as ClassData type.

--- src/contents/trends.ts
+++ src/contents/trends.ts
@@ -1,13 +1,40 @@
+import { Storage } from '@plasmohq/storage';
 import type { PlasmoCSConfig } from 'plasmo';
+
+export interface ClassData {
+  query: {
+    prefix: string;
+    number: string;
+    profFirst?: string;
+    profLast?: string;
+    sectionNumbers?: string[];
+  };
+  semester: string;
+}
+
+// Trends localStorage format:
+// { query: { prefix, number, profFirst, profLast, sectionNumbers: string[] }, semester }
 
 export const config: PlasmoCSConfig = {
   matches: ['https://trends.utdnebula.com/'],
   world: 'MAIN',
 };
 
-const classData = window.localStorage.getItem('planner_v2');
-
 const storage = new Storage();
-storage.set('classData', classData);
 
-console.log(classData);
+export async function fetchFromTrends() {
+  let classes = window.localStorage.getItem('planner_v2');
+  JSON.parse(classes).filter((entry: ClassData) => {
+    // Only add current semester classes with a specific section
+    return (
+      entry.query.prefix &&
+      entry.query.number &&
+      entry.semester &&
+      entry.semester === '26F' &&
+      entry.query.sectionNumbers &&
+      entry.query.sectionNumbers.length > 0
+    );
+  });
+  storage.set('planner_v2_classData', classes);
+  console.log(classes);
+}
